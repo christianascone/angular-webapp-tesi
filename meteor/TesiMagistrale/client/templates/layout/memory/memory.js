e31204e25501c1e31f28a3892dbd280fbcde4ce7
christianascone <ascone.christian@gmail.com>
2017-01-08 01:07:49 +0100

    Initialized cards array with more data and fixed all cards images

--- meteor/TesiMagistrale/client/templates/layout/memory/memory.js
+++ meteor/TesiMagistrale/client/templates/layout/memory/memory.js
@@ -1,25 +1,41 @@
+// When template is created, the array is initialized
+Template.memory.onCreated(function memoryOnCreated() {
+	// counter starts at 0
+	array = [];
+	var lenght = 16;
+	var half_lenght = lenght / 2;
+	for (var i = 0; i < lenght; i++) {
+		var j = i;
+		if (j >= half_lenght) {
+			j -= half_lenght;
+		}
+		// Set image_index to use for src jpg
+		// Flipped and removed are false by default
+		array[i] = {
+			image_index: j,
+			flipped: false,
+			removed: false
+		};
+	}
+});
+
 // Helpers for memory template
 Template.memory.helpers({
 	/**
-	 * Mock array for cards
-	 * @return {Array} mock array
+	 * Gets the cards array
+	 * @return {Array} Cards array
 	 */
 	getArray() {
-		var array = [];
-		var lenght = 16;
-		for (var i = 0; i < lenght; i++) {
-			array[i] = i;
-		}
 		return array;
 	}
 });
 
 // Events for memory template
 Template.memory.events({
-  'click .card-image'(event, t) {
-  	// Get index from selected item
-  	var index = $(event.target).data("value");
-  	// Fade in and out for images
-    $(".card-image-"+index).fadeToggle("slow");
-  },
+	'click .card-image' (event, t) {
+		// Get index from selected item
+		var index = $(event.target).data("value");
+		// Fade in and out for images
+		$(".card-image-" + index).fadeToggle("slow");
+	},
 });
