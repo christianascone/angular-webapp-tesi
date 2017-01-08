22d970f5f3a337c8778bdeea62050493ef47eda0
christianascone <ascone.christian@gmail.com>
2017-01-08 10:51:37 +0100

    Create random memory cards

--- meteor/TesiMagistrale/client/templates/layout/memory/memory.js
+++ meteor/TesiMagistrale/client/templates/layout/memory/memory.js
@@ -1,22 +1,82 @@
-// When template is created, the array is initialized
-Template.memory.onCreated(function memoryOnCreated() {
-	// counter starts at 0
-	array = [];
-	var lenght = 16;
-	var half_lenght = lenght / 2;
-	for (var i = 0; i < lenght; i++) {
+/**
+ * Returns a random int value between min and max
+ * values (included)
+ * http://stackoverflow.com/a/7228322
+ * 
+ * @param  {Int} min Minimum number
+ * @param  {Int} max Maximum number
+ * @return {Int}     A random number between given values
+ */
+function randomIntFromInterval(min,max)
+{
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+/**
+ * Fill and returns an array with every possible index for memory game.
+ * Using the length value passed as param, every value between 0 and length
+ * is appended twice to array
+ * @param  {Int} length Max value to use
+ * @return {[Int]}        Filled array
+ */
+function fillPossibleIndexesWithLenght(length) {
+	// Empty array which will contains the possible
+	// image indexes
+	var possible_indexes = [];
+	// Memory game needs every value appears twice.
+	// The half_length permits to use only length/2 values
+	var half_length = length / 2;
+	// Iterate every card
+	for (var i = 0; i < length; i++) {
 		var j = i;
-		if (j >= half_lenght) {
-			j -= half_lenght;
+		// If the index is greater than half_length, it
+		// must restart from 0
+		if (j >= half_length) {
+			j -= half_length;
 		}
-		// Set image_index to use for src jpg
-		// Flipped and removed are false by default
-		array[i] = {
-			image_index: j,
+		possible_indexes[i] = j;
+	}
+
+	return possible_indexes;
+}
+
+/**
+ * Create a new memory cards array using the possible indexes.
+ * Every card is filled with a random element from possible_indexes array
+ * 
+ * @param  {[Int]} possible_indexes Array containing the possible indexes for Cards
+ * @return {[]}                  Array with created card for memory game
+ */
+function createMemoryCardsArrayWithIndexes(possible_indexes){
+	var cards = [];
+	// Save the initial possible_indexes length
+	var length = possible_indexes.length;
+	for (var i = 0; i < length; i++) {
+		// Gets a random index from possible_indexes array
+		var randomIdx = randomIntFromInterval(0, possible_indexes.length-1);
+		// New card
+		cards[i] = {
+			image_index: possible_indexes[randomIdx],
 			flipped: false,
 			removed: false
 		};
+		// Remove used index from possible_indexes
+		possible_indexes.splice(randomIdx, 1);
 	}
+
+	return cards;
+}
+
+// When template is created, the array is initialized
+Template.memory.onCreated(function memoryOnCreated() {
+	var length = 16;
+	if(length % 2 != 0){
+		console.error("Not even value for memory game.");
+		return;
+	}
+	var possible_indexes = fillPossibleIndexesWithLenght(length);
+
+	cards = createMemoryCardsArrayWithIndexes(possible_indexes);
 });
 
 // Helpers for memory template
@@ -26,7 +86,7 @@ Template.memory.helpers({
 	 * @return {Array} Cards array
 	 */
 	getArray() {
-		return array;
+		return cards;
 	}
 });
 
