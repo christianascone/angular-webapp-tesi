fc58272263fccd4daade861452aafd9f91fa8691
christianascone <ascone.christian@gmail.com>
2017-01-10 18:21:04 +0100

    Animated function for my points and instance fixes

--- meteor/TesiMagistrale/client/templates/helpers/my_points/my_points.js
+++ meteor/TesiMagistrale/client/templates/helpers/my_points/my_points.js
@@ -1,7 +1,16 @@
+var POINTS_READY = "POINTS_READY";
+
+Template.my_points.created = function () {
+    // Setup ready var for this template INSTANCE
+    this.ready = new ReactiveVar(false);
+  };
+
 // Called when the my_points template is rendered
-Template.my_points.onRendered(function () {
+Template.my_points.onRendered(function() {
 	// Make inline svg for filling with color
 	Blaze._globalHelpers.inlineSvg();
+	// After rendering, the ready var is true
+	Template.instance().ready.set(true);
 });
 
 // Helpers functions for my points template
@@ -11,13 +20,40 @@ Template.my_points.helpers({
 	 * 
 	 * @return {Int} Current points
 	 */
-	playerPoints(player) {
+	playerPoints(player, animate) {
+		// If instance is not ready, returns
+		var readyVar = Template.instance().ready;
+		if(!readyVar || !readyVar.get()){
+			return;
+		}
+		// Logged user control
 		var user = Meteor.user();
 		if (!user) {
 			return;
 		}
-		var userId = user._id;
-		
-		return player.totalScore();
+
+		var totalScore = player.totalScore();
+
+		// If animate is false or not defined, simply setup points text value
+		// with jquery
+		if (!animate) {
+			$('.my_points_counter_' + player._id).each(function() {
+				$(this).text(totalScore);
+			});
+			return;
+		}
+
+		// Otherwise, use a great animation
+		$('.my_points_counter_' + player._id).each(function() {
+			$(this).prop('Counter', 0).animate({
+				Counter: totalScore
+			}, {
+				duration: 4000,
+				easing: 'swing',
+				step: function(now) {
+					$(this).text(Math.ceil(now));
+				}
+			});
+		});
 	}
 });
