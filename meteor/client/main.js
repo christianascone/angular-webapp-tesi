2b27b044509ca3ba2e4e2ddaca203e2c8cfc8a9b
Christian Ascone <ascone.christian@gmail.com>
2017-02-02 16:04:16 +0100

    Hide/show setup button (based on private debug setting)

--- meteor/client/main.js
+++ meteor/client/main.js
@@ -15,6 +15,19 @@ dialogPolyfill = require('dialog-polyfill');
 
 import './main.html';
 
+Template.main.onCreated(function mainOnCreated() {
+	// Set default debug value as false (waiting to read it from server)
+	this.isDebug = new ReactiveVar(false);
+	// Read isDebug reactive var in variable, so it is also visible
+	// inside async block
+	var isDebugReactiveVar = Template.instance().isDebug;
+	// Check debug value on server
+	Meteor.call("isDebug", function(err, response) {
+		// Update reactive var
+		isDebugReactiveVar.set(response);
+	});
+});
+
 // Helpers for logout template
 Template.main.helpers({
 	/**
@@ -75,6 +88,13 @@ Template.main.helpers({
 		}
 
 		return loggedPlayer;
+	},
+	/**
+	 * Check if it's in debug environment
+	 * @return {Boolean} Private Debug setting
+	 */
+	isDebug(){
+		return Template.instance().isDebug.get();
 	}
 });
 
