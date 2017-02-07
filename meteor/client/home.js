d907272856ced1609a89976dd16a518c8bf5231c
Christian Ascone <ascone.christian@gmail.com>
2017-02-07 23:59:22 +0100

    Removed icons from header and nabber for NOT fully gasified environment

---
+++ meteor/client/home.js
@@ -0,0 +1,16 @@
+Template.home.helpers({
+	/**
+	 * Returns if it is the fully gamified environment or not
+	 * @return {Boolean} True, if the environment is full, or False if it's minimal
+	 */
+	isFullEnvironment() {
+		var FULLY_GAMIFIED = true;
+		var publicSettings = Meteor.settings.public;
+		if(!publicSettings.ENVIRONMENT || publicSettings.ENVIRONMENT.FULL == undefined){
+			FULLY_GAMIFIED = true;
+		}else{
+			FULLY_GAMIFIED = publicSettings.ENVIRONMENT.FULL;
+		}
+		return FULLY_GAMIFIED;
+	}
+});
