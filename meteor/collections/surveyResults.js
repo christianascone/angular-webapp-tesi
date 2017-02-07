d98d83c1eb48762d716772c96962b8f8c137c83c
Christian Ascone <ascone.christian@gmail.com>
2017-02-07 15:22:17 +0100

    SurveyResults list by bias function

--- meteor/collections/surveyResults.js
+++ meteor/collections/surveyResults.js
@@ -15,6 +15,17 @@ SurveyResults.byUserIdAndBias = function(userId, bias) {
 }
 
 /**
+ * Find surveyResults with given bias
+ * @param  {String} bias Bias of survey
+ * @return {[SurveyResults]}      List of found surveyResults
+ */
+SurveyResults.byBias = function(bias) {
+	return SurveyResults.find({
+		bias: bias
+	});
+}
+
+/**
  * Create a new survey result for userId
  * @param  {Int} userId Id of result's user
  * @param  {String} bias  Bias of filled survey (SURVEY_FRAMING_EFFECT_KEY or SURVEY_CERTAINTY_EFFECT_KEY)
