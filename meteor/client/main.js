755adf3c64eb432117ff422fc043881a090bd091
Christian Ascone <ascone.christian@gmail.com>
2017-02-08 00:32:03 +0100

    Refactored material_icon in template

--- meteor/client/main.js
+++ meteor/client/main.js
@@ -122,23 +122,6 @@ Template.main.helpers({
 	}
 });
 
-Template.logout.helpers({
-	/**
-	 * Returns if it is the fully gamified environment or not
-	 * @return {Boolean} True, if the environment is full, or False if it's minimal
-	 */
-	isFullEnvironment() {
-		var FULLY_GAMIFIED = true;
-		var publicSettings = Meteor.settings.public;
-		if(!publicSettings.ENVIRONMENT || publicSettings.ENVIRONMENT.FULL == undefined){
-			FULLY_GAMIFIED = true;
-		}else{
-			FULLY_GAMIFIED = publicSettings.ENVIRONMENT.FULL;
-		}
-		return FULLY_GAMIFIED;
-	}
-});
-
 Template.logout.events({
 	'click .logout' (event, instance) {
 		event.preventDefault();
